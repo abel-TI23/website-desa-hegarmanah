ae467358a6e4cbd46bc5b7363458edb2541c397a
abel-TI23 <abel.hendrik_ti23@nusaputra.ac.id>
2025-07-28 17:54:36 +0700

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,21 +1,19 @@
 // src/lib/firebase.ts
-
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBHeAgIZ0OE_xI7rcn1rIDoaLIL_MpXNsY",
-  authDomain: "hegarmanah-web.firebaseapp.com",
-  projectId: "hegarmanah-web",
-  storageBucket: "hegarmanah-web.firebasestorage.app",
-  messagingSenderId: "G1061810730132",
-  appId: "1:1061810730132:web:053c0544347795bddcb081"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-
 const db = getFirestore(app);
 const storage = getStorage(app);
 const auth = getAuth(app);
